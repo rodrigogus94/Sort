51117fbbbaf8ddf0a339067fa0d050229b6d48d6
rodrigogus94 <rodrigogus@live.com>
2019-12-26 09:46:05 -0300

    commit das telas de receita e despesas

--- app/src/main/java/com/example/sort/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/sort/activity/PrincipalActivity.java
@@ -1,5 +1,6 @@
 package com.example.sort.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,14 +22,22 @@ public class PrincipalActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+       /* FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+
+    public void addReceita(View view){
+        startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void addDespesa(View view){
+        startActivity(new Intent(this, DespesasActivity.class));
     }
 
 }
