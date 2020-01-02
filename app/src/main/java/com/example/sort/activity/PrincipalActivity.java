4fe0c39c38ef94b383ff89448edb84052969bedc
rodrigo <rodrigogus@live.com>
2020-01-02 13:28:42 -0300

    commit update metodo swipe

--- app/src/main/java/com/example/sort/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/sort/activity/PrincipalActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -72,7 +73,7 @@ public class PrincipalActivity extends AppCompatActivity {
         calendarView = findViewById(R.id.calendarView);
         recyclerView = findViewById(R.id.recyclerMovimentos);
         configuraCalendarView();
-
+        swipe();
 
         //Configurar adapter
         adapterMovimentacao = new AdapterMovimentacao(movimentacoes, this);
@@ -83,6 +84,31 @@ public class PrincipalActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapterMovimentacao);
 
 
+    }
+
+    public void swipe(){
+
+        ItemTouchHelper.Callback callback = new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+
+                int dragFlags = ItemTouchHelper.ACTION_STATE_IDLE;
+                int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+                return makeMovementFlags(dragFlags, swipeFlags);
+            }
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                Log.i("swipe", "Item foi arrastado");
+            }
+        };
+
+        new ItemTouchHelper(callback).attachToRecyclerView(recyclerView);
     }
 
     public void recuperarMovimentacoes(){
